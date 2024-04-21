f4bb7f6026de8596a7cbbb2d6e03e5b515c621a0
statiolake <statiolake@gmail.com>
2024-04-22 01:33:16 +0900

    feat: run csrv before launch

--- src/cli/neovim.rs
+++ src/cli/neovim.rs
@@ -1,4 +1,7 @@
+use std::process::{Command, Stdio};
+
 use anyhow::{bail, Result};
+use scopeguard::defer;
 
 use crate::{
     cli::{Args, NeovimArgs},
@@ -12,8 +15,29 @@ pub fn main(args: &Args, neovim_args: &NeovimArgs) -> Result<()> {
         bail!("Neovim not found, build container first.");
     }
 
+    // Run csrv for clipboard support if exists
+    let csrv = Command::new("csrv")
+        .env("CSRV_PORT", "55232")
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .ok();
+
+    if csrv.is_some() {
+        eprintln!("* csrv started");
+    }
+
+    defer! {
+        if let Some(mut csrv) = csrv {
+            let _ = csrv.kill();
+            let _ = csrv.wait();
+            eprintln!("* csrv stopped")
+        }
+    }
+
+    // Run Neovim in container
     let mut args = vec!["nvim".to_string()];
     args.extend(neovim_args.args.clone());
-
     dc.exec(&args)
 }
