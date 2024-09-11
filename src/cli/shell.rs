a98d94e7843d35c688ec41833f449aeceebe2ae6
statiolake <statiolake@gmail.com>
2024-09-12 00:33:41 +0900

    feat: load config from file

--- src/cli/shell.rs
+++ src/cli/shell.rs
@@ -5,15 +5,15 @@ use crate::{
 };
 use miette::{miette, Result, WrapErr};
 
-pub fn main(_config: &Config, args: &Args, shell_args: &ShellArgs) -> Result<()> {
+pub fn main(config: &Config, args: &Args, shell_args: &ShellArgs) -> Result<()> {
     let dc = DevContainer::new(args.workspace_folder.clone());
 
-    let shell = "/usr/bin/zsh";
-    let mut args = vec![shell];
+    let mut args = vec![&*config.shell];
     args.extend(shell_args.args.iter().map(|s| s.as_str()));
     dc.exec(&args).wrap_err(miette!(
         help = "try `dockim build --rebuild` first",
-        "failed to execute `{shell}` on the container"
+        "failed to execute `{}` on the container",
+        config.shell
     ))?;
 
     Ok(())
--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,74 @@
-use miette::Result;
+use std::{fs, path::PathBuf};
+
+use miette::{miette, Context, IntoDiagnostic, Result};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct Config {
-    pub neovim_version: String,
-    pub dotfiles_repository_name: String,
-    pub dotfiles_install_command: String,
-}
+    #[serde(default = "default_shell")]
+    pub shell: String,
 
-impl Config {
-    pub fn load_config() -> Result<Self> {
-        Ok(Default::default())
-    }
+    #[serde(default = "default_neovim_version")]
+    pub neovim_version: String,
+
+    #[serde(default = "default_dotfiles_repository_name")]
+    pub dotfiles_repository_name: String,
+
+    #[serde(default = "default_dotfiles_install_command")]
+    pub dotfiles_install_command: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            neovim_version: "v0.10.0".to_string(),
-            dotfiles_repository_name: "dotfiles".to_string(),
-            dotfiles_install_command: "python3 install.py --force".to_string(),
+            shell: default_shell(),
+            neovim_version: default_neovim_version(),
+            dotfiles_repository_name: default_dotfiles_repository_name(),
+            dotfiles_install_command: default_dotfiles_install_command(),
         }
+    }
+}
+
+fn default_shell() -> String {
+    "/usr/bin/bash".to_string()
+}
+
+fn default_neovim_version() -> String {
+    "v0.10.0".to_string()
+}
+
+fn default_dotfiles_repository_name() -> String {
+    "dotfiles".to_string()
+}
+
+fn default_dotfiles_install_command() -> String {
+    "echo 'no dotfiles install command configured'".to_string()
+}
+
+impl Config {
+    pub fn config_file_path() -> Result<PathBuf> {
+        Ok(dirs::config_dir()
+            .ok_or_else(|| miette!("could not find config directory"))?
+            .join("dockim")
+            .join("config.toml"))
+    }
+
+    pub fn load_config() -> Result<Self> {
+        let path = Self::config_file_path()?;
+
+        if !path.exists() {
+            let config = Config::default();
+            return Ok(config);
+        }
+
+        let contents = fs::read_to_string(&path)
+            .into_diagnostic()
+            .wrap_err("failed to read config file contents")?;
+
+        let config = toml::from_str(&contents)
+            .into_diagnostic()
+            .wrap_err("failed to parse config file")?;
+
+        Ok(config)
     }
 }
